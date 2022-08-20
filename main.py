ec8a25051ac502c89fcad815bf9b901c1c93ea98
gsusin <gabriel.susin@adp.com>
2022-08-20 00:45:02 -0300

    Resultados imprimindo - verificar a veracidade dos mesmos

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from investments import investments
 from installments import installments
 import numpy_financial as npf
 
-
 cashflows = {}
-
 
 def calc_irr():
     data = []
@@ -12,8 +10,19 @@ def calc_irr():
         data.append(amount)
 
     irr = round(npf.irr(data), 2)
-    print("TIR:", irr)
-
+    print(irr)
 
 if __name__ == "__main__":
-    calc_irr()
+
+    index = 0
+    for inv in investments:
+        cashflows[index] = float(inv['amount']) * -1
+        index += 1
+        for inst in installments:
+            if inv['id'] == inst['investment_id']:
+                cashflows[index] = inst['amount']
+                index += 1
+        index = 0
+        calc_irr()
+        cashflows.clear()
+
